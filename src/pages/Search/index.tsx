5601d4434d60272c3b6f14188a6584795036b703
Camila Moura <camilapaleo@gmail.com>
2020-06-14 23:27:41 +0200

    added search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,20 +1,97 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { Section } from '../../styles/shared';
 
-import { Container, Content, Form } from './styles';
+import api, { API_BASE_IMAGE_URL } from '../../services/api';
+
+import { Container, Content, Form, Error } from './styles';
 import Header from '../../components/Header';
 
+interface IResult {
+  poster_path: string | null;
+  overview: string;
+  realese_date: string;
+  original_title: string;
+  original_language: string;
+  title: string;
+  popularity: number;
+  vote_count: number;
+  vote_avanger: number;
+  first_air_date: string;
+  origin_country: string;
+  name: string;
+  original_name: string;
+  id: number;
+}
+
 const Search: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [result, setResult] = useState<IResult[]>([]);
+
+  async function handleSearch(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!query) {
+      setInputError('To be continue, you need add a Search.');
+      return;
+    }
+
+    try {
+      const response = await api.get(`search/multi?query=${query}`);
+
+      setResult(response.data.results);
+      console.log(response.data.results);
+      setQuery('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Sorry, nothing found.');
+    }
+  }
+
   return (
     <>
       <Container>
         <Header />
         <Content>
           <h1>CINE</h1>
-          <Form>
-            <input type="{search}" placeholder="Type here" />
+
+          <Form onSubmit={handleSearch}>
+            <input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Type here"
+            />
+
             <button type="submit">Search</button>
           </Form>
         </Content>
+
+        {inputError && <Error>{inputError}</Error>}
+
+        {result.length > 0 && (
+          <Section>
+            <h1>Results</h1>
+            <ul>
+              {result.map((item) => (
+                <li key={item.id}>
+                  <Link to={`/Detail/search/${query}`}>
+                    {item.poster_path !== null ? (
+                      <img
+                        src={`${API_BASE_IMAGE_URL}w342${item.poster_path}`}
+                        alt="{item.title}"
+                      />
+                    ) : (
+                      <h1>ops</h1>
+                    )}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Section>
+        )}
       </Container>
     </>
   );
--- src/pages/Search/styles.ts
+++ src/pages/Search/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   height: 100%;
@@ -16,7 +16,7 @@ export const Content = styled.div`
   }
 `;
 
-export const Form = styled.div`
+export const Form = styled.form`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -31,6 +31,8 @@ export const Form = styled.div`
     border-top-left-radius: 50px;
     background: #e5e5e5;
     border-right: 0;
+    color: #524b4b;
+    font-size: 20px;
 
     &::placeholder {
       color: #524b4b;
@@ -40,7 +42,17 @@ export const Form = styled.div`
   }
 
   button {
-    padding: 0 30px;
-    margin-left: -20px;
+    padding: 0 40px;
+    margin-left: -50px;
   }
+`;
+
+export const Error = styled.span`
+  /* display: block; */
+  color: #c53030;
+  margin-top: 8px;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
