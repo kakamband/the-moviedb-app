ee507c506136d10f7b09eb570652d3744919e184
Camila Moura <camilapaleo@gmail.com>
2020-06-15 22:30:20 +0200

    created media componet

---
+++ src/components/Media/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import notfound from '../../assets/notfound.svg';
+import { API_BASE_IMAGE_URL } from '../../services/api';
+
+export interface Item {
+  poster_path: string | null;
+  title: string;
+  id: number;
+}
+
+interface IMedia {
+  type: string;
+  item: Item;
+}
+
+const Media: React.FC<IMedia> = ({ type, item }) => {
+  return (
+    <Link to={`/detail/${type}/${item.id}`}>
+      {item.poster_path !== null ? (
+        <img
+          src={`${API_BASE_IMAGE_URL}w342${item.poster_path}`}
+          alt="{item.title}"
+        />
+      ) : (
+        <img src={notfound} alt="notfound" />
+      )}
+    </Link>
+  );
+};
+
+export default Media;
