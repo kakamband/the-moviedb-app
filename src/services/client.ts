379bc240cf2ac6b9b5bea4427215d10e0485006f
Camila Moura <camilapaleo@gmail.com>
2020-06-15 17:38:29 +0200

    fixed name

--- src/services/client.ts
+++ src/services/client.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const API_TOKEN = process.env.REACT_APP_API_TOKEN || 'api-token';
 axios.defaults.headers.common = { Authorization: `Bearer ${API_TOKEN}` };
 
-const api = axios.create({
+const client = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
 });
 
-export default api;
+export default client;
