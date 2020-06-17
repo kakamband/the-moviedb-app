6cf0ad87d9e8574b532ad0e8860d3454e2bc7582
Camila Moura <camilapaleo@gmail.com>
2020-06-17 15:55:12 +0200

    it became the cleared code

--- src/components/PopularMovies/index.tsx
+++ src/components/PopularMovies/index.tsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
-
 import { getPopularMovies } from '../../services/api';
 
 import { Section } from '../../styles/shared';
 import { Item } from '../Media';
+
 import MediaCarousel from '../MediaCarousel';
 
 const PopularMovies: React.FC = () => {
   const [movies, setMovies] = useState<Item[]>([]);
 
   useEffect(() => {
+    //It get popularMovie from Api endpoint(services/api.ts)
     async function loadPopularMovies() {
       const popularMovies = await getPopularMovies();
       setMovies(
